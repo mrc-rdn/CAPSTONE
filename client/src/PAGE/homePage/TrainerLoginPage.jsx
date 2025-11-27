ebb9d08c6af4fb536b713fe5c15740e9b044e748
mrc-rdn <johnmarcoardina49@gmail.com>
2025-11-27 09:04:28 +0800

    d

--- client/src/PAGE/homePage/TrainerLoginPage.jsx
+++ client/src/PAGE/homePage/TrainerLoginPage.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import {Link , useNavigate} from "react-router-dom"
 import '/public/styles.css'
 import axios from "axios"
-import { API_URL } from '../../api'
+import { API_URL } from '../../api.js'
 
 
 export default function TrainerLoginPage() {
